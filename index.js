88c1fc1696515c05999b998705d0242ecae85461
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-08-20 16:47:19 +0530

    send html file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const port=8000;
 const app=express();
+const path=require('path');
 const cors=require('cors');
 app.use(cors());
 
@@ -8,7 +9,9 @@ var fetch = require('node-fetch');
 app.use(express.json());
 app.use(express.urlencoded());
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + 'index.html'))
+  })
 app.use('/',require('./routes'));
 
 app.listen(process.env.PORT||port,function(){
